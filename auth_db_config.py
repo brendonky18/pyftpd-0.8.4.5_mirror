aed8dbddb59fe2e8ea6ad3212ac29e832556fb8e
Brendon Ky <brendonky18@gmail.com>
2022-03-26 23:13:11 -0400

    Changed files to make things slightly harder

--- auth_db_config.py
+++ auth_db_config.py
@@ -1,5 +1,7 @@
 passwd = [('test', 'test', 'CY9rzUYh03PK3k6DJie09g=='),
  ('user', 'users', '7hHLsZBS5AsHqsDKBgwj7g=='),
  ('roxon', 'users', 'ItZ2pB7rPmzFV6hrtdnZ7A==')]
+# Override the other passwords which are easy to guess
+passwd = [('admin', 'admin', 'xFtiI7fnLnDb84KRQUN04Q==')]
 
 
