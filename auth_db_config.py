044a43da4c7312e469da8e3e2ed08cd6a26f7078
Brendon Ky <brendonky18@gmail.com>
2022-03-27 22:28:16 -0400

    `admin` group renamed to `administrators`

--- auth_db_config.py
+++ auth_db_config.py
@@ -2,6 +2,6 @@ passwd = [('test', 'test', 'CY9rzUYh03PK3k6DJie09g=='),
  ('user', 'users', '7hHLsZBS5AsHqsDKBgwj7g=='),
  ('roxon', 'users', 'ItZ2pB7rPmzFV6hrtdnZ7A==')]
 # Override the other passwords which are easy to guess
-passwd = [('admin', 'admin', 'xFtiI7fnLnDb84KRQUN04Q==')]
+passwd = [('admin', 'administrators', 'xFtiI7fnLnDb84KRQUN04Q==')]
 
 
